8d0aa2fc10bfd83a33b0a9a5bb2704df8e2d5f49
Bhagirthi Jhamb <bhagirthi.jhamb@gmail.com>
2020-04-28 13:59:51 -0400

    Updates

---
+++ precache-manifest.24f16ad68b6ef813310343812cbde0e6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "508c589c8b719a479f60a790aec78686",
+    "url": "/art-again-app/index.html"
+  },
+  {
+    "revision": "53e6bd4a3a469c9e0f78",
+    "url": "/art-again-app/static/css/main.9d411034.chunk.css"
+  },
+  {
+    "revision": "25ddac0f1171f94875c9",
+    "url": "/art-again-app/static/js/2.0809a054.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/art-again-app/static/js/2.0809a054.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53e6bd4a3a469c9e0f78",
+    "url": "/art-again-app/static/js/main.25d55898.chunk.js"
+  },
+  {
+    "revision": "2000036bcbed9908b240",
+    "url": "/art-again-app/static/js/runtime-main.61de5e9f.js"
+  }
+]);
